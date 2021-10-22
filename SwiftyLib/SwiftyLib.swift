38aea248d7653ed2ae584f356d715f56c5851950
thuytien <tien280198@gmail.com>
2021-10-22 09:31:11 +0700

    Added SwiftyLib.podspec

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Pham Thuy Tien on 22/10/2021.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
